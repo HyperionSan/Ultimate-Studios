a514f48d9557eeca84b167d2618eeab3312baaf5
HyperionSan <varun0904@gmail.com>
2024-06-11 22:33:58 +0530

    End of the day commit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
-function setup(){
+function setuo(){
 	createCanvas(200,200);
+	print("Hello!");
 }
 
 function draw(){
-	background(0);
-	fill(255,0,0);
-	rect(100,100,50,50);
+	background(200);
+	noStroke();
+	ellipse(100,100,50,55,10,12);
 }
+
